55e7e1dbcfe00179eaeb26f6ac5c09735a9c8f38
Takuro Ashie <ashie@clear-code.com>
2017-07-19 16:11:48 +0900

    Add registry listener

--- src/main.c
+++ src/main.c
@@ -2,6 +2,40 @@
 #include <stdlib.h>
 #include <wayland-client.h>
 
+void
+global_registory_handler(void *data,
+                         struct wl_registry *wl_registry,
+                         uint32_t name,
+                         const char *interface,
+                         uint32_t version)
+{
+    printf("global_registory_handler: %s, name %d, version %d\n",
+           interface, name, version);
+}
+
+void
+global_registory_remover(void *data,
+                         struct wl_registry *wl_registry,
+                         uint32_t name)
+{
+    printf("global_registory_remover: name %d\n", name);
+}
+
+void
+print_server_info(struct wl_display *display)
+{
+    const struct wl_registry_listener registry_listener = {
+        global_registory_handler,
+        global_registory_remover
+    };
+    struct wl_registry *registry = wl_display_get_registry(display);
+    wl_registry_add_listener(registry, &registry_listener, NULL);
+    int n_events = wl_display_dispatch(display);
+    printf("wl_display_dispatch: n_events %d\n", n_events);
+    n_events = wl_display_roundtrip(display);
+    printf("wl_display_roundtrip: n_events %d\n", n_events);
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -11,6 +45,8 @@ main(int argc, char *argv[])
         return 1;
     }
     printf("Connected to the display\n");
+
+    print_server_info(display);
 
     wl_display_disconnect(display);
     printf("Disconnected from the display\n");
